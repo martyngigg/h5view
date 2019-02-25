99bc2dc89c20cadb645bf02a5b4229af14c64f53
Martyn Gigg <martyn.gigg@gmail.com>
2019-02-25 21:31:03 +0000

    Enable C++14 and sanitizers on debug builds

--- cmake/Modules/CompilerConfig.cmake
+++ cmake/Modules/CompilerConfig.cmake
@@ -5,6 +5,12 @@
 # and delegates the actual configuration to the appropriate file in the
 # compilers subdirectory
 # ------------------------------------------------------------------------------
+
+# Required C++14
+set(CMAKE_CXX_STANDARD 14)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_EXTENSIONS OFF)
+
 # GCC >= 4.2
 if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND
     CMAKE_CXX_COMPILER_VERSION VERSION_LESS 4.2)
