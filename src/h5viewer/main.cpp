2e19c0b5715e9f95dbc552cd968ff669cd6133a2
Martyn Gigg <martyn.gigg@gmail.com>
2019-10-11 20:52:09 +0100

    Add error handling to main

--- src/h5viewer/main.cpp
+++ src/h5viewer/main.cpp
@@ -1,7 +1,15 @@
+#include <iostream>
+
 #include "h5view/h5view.hpp"
 
+using std::cerr;
 using h5view::root_group_name;
 
-int main(int /*argc*/, char **argv) {
+int main(int argc, char **argv) {
+  if(argc != 2) {
+    cerr << "Usage: h5viewer filepath\n";
+    return 1;
+  }
   root_group_name(argv[1]);
+  return 0;
 }
