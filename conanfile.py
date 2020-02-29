d76c7836fa4471aeb96c2f6607afc6fae67d456b
Martyn Gigg <martyn.gigg@gmail.com>
2020-02-29 19:40:42 +0000

    Add conanfile.py to replace .txt

---
+++ conanfile.py
@@ -0,0 +1,19 @@
+from conans import ConanFile
+
+
+class H5ViewConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    requires = [
+        "hdf5/1.10.5-dm1@ess-dmsc/stable", "Python/3.8.0",
+        ("zlib/1.2.11", "override"),
+    ]
+    build_requires = "pybind11/2.3.0@conan/stable"
+    generators = "cmake"
+    default_options = {
+        "*:shared": True
+    }
+
+    def imports(self):
+        self.copy("python*", dst="bin", src="bin")
+        self.copy("*.so*", dst="lib", src="lib")
+        self.copy("python3.8/*", dst="lib", src="lib")
